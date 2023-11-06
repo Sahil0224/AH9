842ce6b8fdb5407c0b47fa4a51f48d57439e5c65
Patel <sahil.patel@fiserv.com>
2023-11-06 14:08:58 -0500

    Finished Ah9.

--- ECSource.java
+++ ECSource.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 class Node<T> {
   T val;
   Node<T> next;
@@ -10,15 +12,44 @@ class Node<T> {
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
-    return true;
+
+    // initialize a HashSet to store the values
+    HashSet<T> values = new HashSet<>();
+
+    // loop through the list while head is not null
+    while(head != null)
+    {
+      // add the current value to the set
+      values.add(head.val);
+      
+      // move to the next node
+      head = head.next;
+    }
+
+    // return true if size equals to 1
+    return values.size() == 1;
   }
 
   public static void main(String[] args) {
         Node<String> z = new Node<>("z");
         // z
-
         // Printing solution
         System.out.println((ECSource.isUnivalueList(z)));
+
+        Node<Integer> u = new Node<>(2);
+    
+        Node<Integer> v = new Node<>(2);
+        Node<Integer> w = new Node<>(3);
+        Node<Integer> x = new Node<>(3);
+        Node<Integer> y = new Node<>(2);
+
+        u.next = v;
+        v.next = w;
+        w.next = x;
+        x.next = y;
+
+        // 2 -> 2 -> 3 -> 3 -> 2
+
+        System.out.println(ECSource.isUnivalueList(u));
     }
 }
--- Source.java
+++ Source.java
@@ -1,4 +1,5 @@
-class Node<T> {
+class Node<T> 
+{
     T val;
     Node<T> next;
     
@@ -6,21 +7,48 @@ class Node<T> {
       this.val = val;
       this.next = null;
     }
-  }
+}
   
   class Source {
     public static <T> Node<T> reverseList(Node<T> head) {
-      // todo
+      // Check if the list is empty or has just one node then return head.
+
+      if (head == null || head.next == null)
+      {
+        return head;
+      }
+      
+      // Initialize a pointer 
+      Node<T> prev = null; // previous node
+      Node<T> current = head; // current node
+      Node<T> next; // next node
+
+      
+      // Iterate through the list until head is null
+      while(current != null)
+      {
+        // store the next node as current
+        next = current.next;
+        // make the current node to the previous node
+        current.next = prev;
+        // move the previous and current pointers forward
+        prev = current;
+        current = next;
+      }
+      // made the head to the previous node
+      head = prev;
+      // return head
       return head;
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Node<String> x = new Node<>("x");
         Node<String> y = new Node<>("y");
     
         x.next = y; // x -> y
     
-        reverseList(x); // y -> x
+       // reverseList(x); // y -> x
 
         // Printing solution
         Node<String> head = Source.reverseList(x);
@@ -31,3 +59,4 @@ class Node<T> {
         }
     }
   }
+  
